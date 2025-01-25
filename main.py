8842b68255a8df5e1ba18f0f4d32ed526b371ae3
leolulu_sad_python <348699103@qq.com>
2025-01-25 22:37:25 +0800

    初版可以上线了

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from server.web_server import app
 
 if __name__ == "__main__":
-    app.run(debug=True, port=1131)
+    app.run(debug=False, port=1131)
--- server/web_server.py
+++ server/web_server.py
@@ -6,20 +6,58 @@ app = Flask(__name__)
 sm = SessionManager()
 
 
-@app.route("/", methods=["GET"])
+@app.route("/help", methods=["GET"])
 def home():
-    return f"可接受请求参数:<br><br>id: 会话id, 不提供则自动生成<br>system_message: 系统消息, 不提供则不使用系统消息<br>user_message: 用户消息(必填)"
+    content_lines = [
+        "可接受请求参数:",
+        "",
+        "id : 会话id, 不提供则自动生成",
+        "system_message : 系统消息, 不提供则不使用系统消息",
+        "preserve : 是否对于相同的会话id保留历史记录",
+        "user_message : 用户消息(必填)",
+    ]
+    return "<br>".join(content_lines)
+
+
+@app.route("/inspect", methods=["GET"])
+def inspect_all_messages():
+    return jsonify([m.messages.messages for m in sm.pool.values()])
+
+
+def _chat_using_parameters(id, system_message, user_message, preserve):
+    if not user_message:
+        return "缺少必填参数: user_message", 400
+
+    preserve = preserve.lower() in ["true", "1", "yes"] if preserve else False
+
+    session = sm.get_or_create_session(id)
+    if system_message:
+        session.adjust_system_message(system_message)
+
+    if preserve:
+        answer = session.chat_preserving_history(user_message)
+    else:
+        answer = session.chat_once(user_message)
+
+    return str(answer)
 
 
 @app.route("/", methods=["POST"])
-def submit():
+def process_chat_request_port():
     payload = request.get_json()
     id = payload.get("id")
     system_message = payload.get("system_message")
     user_message = payload["user_message"]
+    preserve = payload.get("preserve")
 
-    session = sm.get_or_create_session(id)
-    session.adjust_system_message(system_message)
-    answer = session.chat_once(user_message)
+    return _chat_using_parameters(id, system_message, user_message, preserve)
 
-    return str(answer)
+
+@app.route("/", methods=["GET"])
+def process_chat_request_get():
+    id = request.args.get("id")
+    system_message = request.args.get("system_message")
+    user_message = request.args.get("user_message")
+    preserve = request.args.get("preserve")
+
+    return _chat_using_parameters(id, system_message, user_message, preserve)
